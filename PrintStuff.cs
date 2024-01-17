83e4eadbd00cced49fd58ac4cce66ace1576e8dc
Logan McNatt <logan.m.mcnatt@gmail.com>
2024-01-16 20:06:48 -0700

    Add project files.

---
+++ PrintStuff.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.SymbolStore;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoreCSharpFun
+{
+    internal class PrintStuff
+    {
+        private string language;
+        public PrintStuff(string temp) {
+            language = temp;
+        }
+
+        public void PrintName(string name)
+        {
+            if (language == "EN")
+            {
+                System.Console.WriteLine("Hello, " + name + "!");
+            }
+            else if (language == "PT")
+            {
+                System.Console.WriteLine("Oi, " + name + "!");
+            }
+            else if (language == "SP")
+            {
+                System.Console.WriteLine("Hola, " + name + "!");
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using MoreCSharpFun;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        PrintStuff ps = new PrintStuff("EN");
+
+        string name = "";
+
+        System.Console.WriteLine("Please enter your name");
+        name = System.Console.ReadLine();
+
+        ps.PrintName(name);
+    }
+}
